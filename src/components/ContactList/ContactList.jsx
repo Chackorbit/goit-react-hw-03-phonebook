edd541169942a65b36fd83ad0f7b9dcb8c77bd57
Dmitriy Vasylchuk <dmvs.9898@gmail.com>
2022-03-17 11:20:09 +0200

    сделал из контакт лист простой функциональный компонент

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,33 +3,32 @@ import React from 'react';
 import ContactItem from '../ContactItem/ContactItem';
 import { nanoid } from 'nanoid';
 
-export default class ContactList extends React.Component {
-  state = {};
+const ContactList = ({ findByName, deleteContact }) => {
+  const fullList = findByName();
 
-  render() {
-    const fullList = this.props.findByName();
+  return (
+    <div className={s.container}>
+      <ul className={s.ul}>
+        {fullList.length < 1 ? (
+          <p>We dont have contacts</p>
+        ) : (
+          fullList.map(el => (
+            <ContactItem
+              key={nanoid()}
+              name={el.name}
+              number={el.number}
+              id={el.id}
+              deleteContact={deleteContact}
+            />
+          ))
+        )}
+      </ul>
+    </div>
+  );
+};
 
-    return (
-      <div className={s.container}>
-        <ul className={s.ul}>
-          {fullList.length < 1 ? (
-            <p>We dont have contacts</p>
-          ) : (
-            fullList.map(el => (
-              <ContactItem
-                key={nanoid()}
-                name={el.name}
-                number={el.number}
-                id={el.id}
-                deleteContact={this.props.deleteContact}
-              />
-            ))
-          )}
-        </ul>
-      </div>
-    );
-  }
-}
+export default ContactList;
+
 // Statistics.propTypes = {
 //   good: propTypes.number,
 //   neutral: propTypes.number,
